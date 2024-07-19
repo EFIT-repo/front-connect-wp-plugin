88b8502ae5c428f2ebec0a2b3589cc1ae4e60816
Petr Efimov (Shkoda) <apple-i-shop@yandex.ru>
2024-07-19 18:35:40 +0300

    Init commit

---
+++ front-connect.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Front Connect
+Description: Подключайте веб-приложения сделанные на React, Vue или Angular к WordPress в пару кликов!
+Version: 1.0
+Author: @efimov_it
+*/
+
+// Защита от прямого доступа
+if (!defined('ABSPATH')) {
+    exit;
+}
